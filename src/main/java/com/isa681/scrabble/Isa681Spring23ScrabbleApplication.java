339451b5c0f99e97beeda3b7338285b04a8e0d8a
Nidhi Chowdhry <nidhichowdhry@nidhis-mbp.mynetworksettings.com>
2023-02-28 23:48:25 -0500

    Creating Scrabble Project

---
+++ src/main/java/com/isa681/scrabble/Isa681Spring23ScrabbleApplication.java
@@ -0,0 +1,13 @@
+package com.isa681.scrabble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Isa681Spring23ScrabbleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Isa681Spring23ScrabbleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/isa681/scrabble/Isa681Spring23ScrabbleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isa681.scrabble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Isa681Spring23ScrabbleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
