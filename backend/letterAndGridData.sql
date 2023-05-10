4ef2e43c1d598d326c56fde6380f479c07cf60fd
akhilamantri <manthriakhila1@gmail.com>
2023-05-10 03:02:48 -0400

    Added letters and grid data to the backend sql script

---
+++ backend/letterAndGridData.sql
@@ -0,0 +1,250 @@
+INSERT INTO `scrabble`.`grid`(x_coordinate,y_coordinate,count)
+
+values (0,0,1);
+
+INSERT INTO `scrabble`.`grid`(x_coordinate,y_coordinate,count)
+
+values (0,1,1);
+
+INSERT INTO `scrabble`.`grid`(x_coordinate,y_coordinate,count)
+
+values (0,2,1);
+
+INSERT INTO `scrabble`.`grid`(x_coordinate,y_coordinate,count)
+
+values (0,3,1);
+
+INSERT INTO `scrabble`.`grid`(x_coordinate,y_coordinate,count)
+
+values (0,4,1);
+
+INSERT INTO `scrabble`.`grid`(x_coordinate,y_coordinate,count)
+
+values (0,5,1);
+
+INSERT INTO `scrabble`.`grid`(x_coordinate,y_coordinate,count)
+
+values (1,0,1);
+
+INSERT INTO `scrabble`.`grid`(x_coordinate,y_coordinate,count)
+
+values (1,1,1);
+
+INSERT INTO `scrabble`.`grid`(x_coordinate,y_coordinate,count)
+
+values (1,2,1);
+
+INSERT INTO `scrabble`.`grid`(x_coordinate,y_coordinate,count)
+
+values (1,3,1);
+
+INSERT INTO `scrabble`.`grid`(x_coordinate,y_coordinate,count)
+
+values (1,4,1);
+
+INSERT INTO `scrabble`.`grid`(x_coordinate,y_coordinate,count)
+
+values (1,5,1);
+
+INSERT INTO `scrabble`.`grid`(x_coordinate,y_coordinate,count)
+
+values (2,0,1);
+
+INSERT INTO `scrabble`.`grid`(x_coordinate,y_coordinate,count)
+
+values (2,1,1);
+
+INSERT INTO `scrabble`.`grid`(x_coordinate,y_coordinate,count)
+
+values (2,2,1);
+
+INSERT INTO `scrabble`.`grid`(x_coordinate,y_coordinate,count)
+
+values (2,3,1);
+
+INSERT INTO `scrabble`.`grid`(x_coordinate,y_coordinate,count)
+
+values (2,4,1);
+
+INSERT INTO `scrabble`.`grid`(x_coordinate,y_coordinate,count)
+
+values (2,5,1);
+
+INSERT INTO `scrabble`.`grid`(x_coordinate,y_coordinate,count)
+
+values (3,0,1);
+
+INSERT INTO `scrabble`.`grid`(x_coordinate,y_coordinate,count)
+
+values (3,1,1);
+
+INSERT INTO `scrabble`.`grid`(x_coordinate,y_coordinate,count)
+
+values (3,2,1);
+
+INSERT INTO `scrabble`.`grid`(x_coordinate,y_coordinate,count)
+
+values (3,3,1);
+
+INSERT INTO `scrabble`.`grid`(x_coordinate,y_coordinate,count)
+
+values (3,4,1);
+
+INSERT INTO `scrabble`.`grid`(x_coordinate,y_coordinate,count)
+
+values (3,5,1);
+
+INSERT INTO `scrabble`.`grid`(x_coordinate,y_coordinate,count)
+
+values (4,0,1);
+
+INSERT INTO `scrabble`.`grid`(x_coordinate,y_coordinate,count)
+
+values (4,1,1);
+
+INSERT INTO `scrabble`.`grid`(x_coordinate,y_coordinate,count)
+
+values (4,2,1);
+
+INSERT INTO `scrabble`.`grid`(x_coordinate,y_coordinate,count)
+
+values (4,3,1);
+
+INSERT INTO `scrabble`.`grid`(x_coordinate,y_coordinate,count)
+
+values (4,4,1);
+
+INSERT INTO `scrabble`.`grid`(x_coordinate,y_coordinate,count)
+
+values (4,5,1);
+
+INSERT INTO `scrabble`.`grid`(x_coordinate,y_coordinate,count)
+
+values (5,0,1);
+
+INSERT INTO `scrabble`.`grid`(x_coordinate,y_coordinate,count)
+
+values (5,1,1);
+
+INSERT INTO `scrabble`.`grid`(x_coordinate,y_coordinate,count)
+
+values (5,2,1);
+
+INSERT INTO `scrabble`.`grid`(x_coordinate,y_coordinate,count)
+
+values (5,3,1);
+
+INSERT INTO `scrabble`.`grid`(x_coordinate,y_coordinate,count)
+
+values (5,4,1);
+
+INSERT INTO `scrabble`.`grid`(x_coordinate,y_coordinate,count)
+
+values (5,5,1);
+
+
+INSERT INTO letters(alphabet,letter_score,count)
+
+values ('A',6,20);
+
+INSERT INTO letters(alphabet,letter_score,count)
+
+values ('B',3,10);
+
+INSERT INTO letters(alphabet,letter_score,count)
+
+values ('C',6,5);
+
+INSERT INTO letters(alphabet,letter_score,count)
+
+values ('D',2,12);
+
+INSERT INTO letters(alphabet,letter_score,count)
+
+values ('E',8,1);
+
+INSERT INTO letters(alphabet,letter_score,count)
+
+values ('F',3,3);
+
+INSERT INTO letters(alphabet,letter_score,count)
+
+values ('G',10,4);
+
+INSERT INTO letters(alphabet,letter_score,count)
+
+values ('H',7,5);
+
+INSERT INTO letters(alphabet,letter_score,count)
+
+values ('I',4,2);
+
+INSERT INTO letters(alphabet,letter_score,count)
+
+values ('J',6,10);
+
+INSERT INTO letters(alphabet,letter_score,count)
+
+values ('K',6,10);
+
+INSERT INTO letters(alphabet,letter_score,count)
+
+values ('L',5,3);
+
+INSERT INTO letters(alphabet,letter_score,count)
+
+values ('M',7,6);
+
+INSERT INTO letters(alphabet,letter_score,count)
+
+values ('N',3,2);
+
+INSERT INTO letters(alphabet,letter_score,count)
+
+values ('O',8,1);
+
+INSERT INTO letters(alphabet,letter_score,count)
+
+values ('P',3,9);
+
+INSERT INTO letters(alphabet,letter_score,count)
+
+values ('Q',1,11);
+
+INSERT INTO letters(alphabet,letter_score,count)
+
+values ('R',9,9);
+
+INSERT INTO letters(alphabet,letter_score,count)
+
+values ('S',6,2);
+
+INSERT INTO letters(alphabet,letter_score,count)
+
+values ('T',8,3);
+
+INSERT INTO letters(alphabet,letter_score,count)
+
+values ('U',2,5);
+
+INSERT INTO letters(alphabet,letter_score,count)
+
+values ('V',4,5);
+
+INSERT INTO letters(alphabet,letter_score,count)
+
+values ('W',1,1);
+
+INSERT INTO letters(alphabet,letter_score,count)
+
+values ('X',2,12);
+
+INSERT INTO letters(alphabet,letter_score,count)
+
+values ('Y',3,3);
+
+INSERT INTO letters(alphabet,letter_score,count)
+
+values ('Z',6,7);
+
+
