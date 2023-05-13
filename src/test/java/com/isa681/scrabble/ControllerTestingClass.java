93c9f7d15f808a8e6330df1e73e0d1e27d89be2b
Sai Srujan Dandyala <dsaisrujan@gmail.com>
2023-05-12 21:09:00 -0400

    Limited grid input size to max 1 and changed the entered alphabets to uppercase

---
+++ src/test/java/com/isa681/scrabble/ControllerTestingClass.java
@@ -0,0 +1,12 @@
+package com.isa681.scrabble;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ControllerTestingClass {
+    @Test
+    public void TestMethod(){
+        assertTrue(true);
+    }
+}
