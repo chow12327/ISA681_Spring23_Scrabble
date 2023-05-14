b6ad84af379abe069ff34f925474f7ce6a4791c6
Sai Srujan Dandyala <dsaisrujan@gmail.com>
2023-05-13 22:41:10 -0400

    service testing

--- src/test/java/com/isa681/scrabble/service/GetGameInfoServiceTest.java
+++ src/test/java/com/isa681/scrabble/service/GetGameInfoServiceTest.java
@@ -1,6 +1,9 @@
 package com.isa681.scrabble.service;
 
 import com.isa681.scrabble.entity.Game;
+import com.isa681.scrabble.service.GetGameInfoService;
+import com.isa681.scrabble.dao.PlayerRepository;
+import com.isa681.scrabble.entity.Player;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -19,12 +22,10 @@ import static org.mockito.Mockito.when;
 
 @SpringBootTest(classes = GetGameInfoServiceTest.class)
 class GetGameInfoServiceTest {
-//    @Mock
-//    Game game;
+    @Mock
+    PlayerRepository playerRepository;
 //    @InjectMocks
-//    GetGameInfoService getGameInfoService;
-
-
+    GetGameInfoService getGameInfoService;
     @Test
     void getHistoricGameInfo() throws ParseException {
         List<Game> games = new ArrayList<>();
@@ -47,7 +48,7 @@ class GetGameInfoServiceTest {
         assertEquals(1,game.getId());
         assertEquals(true,game.getIsFinished());
         assertEquals(false,game.getIsDraw());
-
+        System.out.println("testing complete getHistoricGameInfo");
 
     }
 
@@ -72,10 +73,30 @@ class GetGameInfoServiceTest {
         assertEquals(2,game.getId());
         assertEquals(false,game.getIsFinished());
         assertEquals(false,game.getIsDraw());
+        System.out.println("testing complete getActiveGameInfo");
     }
 
+
+    public List<Player> players;
     @Test
     void getPlayers() {
+        List<Player> players = new ArrayList<Player>();
 
+        char[] pwd = {'r', 's', 't', 'u', 'v'};
+        Player player = new Player();
+        player.setFirstName("first");
+        player.setLastName("last");
+        player.setUserName("user");
+        player.setPassword(pwd);
+        players.add(player);
+
+        when(playerRepository.findAll()).thenReturn(players); //mocking is applied
+
+        assertEquals("first",player.getFirstName());
+        assertEquals("last",player.getLastName());
+        assertEquals("user",player.getUserName());
+        assertEquals(pwd,player.getPassword());
+
+        System.out.println("testing complete all player data is matched as expected");
     }
 }
